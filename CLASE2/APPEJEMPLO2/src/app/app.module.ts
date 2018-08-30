6c6c12e2b660a360aba10d1e766bfc3a37162a6c
Leandro Pollola <LPollola@coasa.com.ar>
2018-08-30 20:56:20 -0300

    RUTEO

--- CLASE2/APPEJEMPLO2/src/app/app.module.ts
+++ CLASE2/APPEJEMPLO2/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
+import { LoginComponent } from './componentes/login/login.component';
+// RUTEO
+import { Routes, RouterModule} from '@angular/router';
+
+// RUTEO
+const miRuteo = [
+  {path:'bienvenida', component:BienvenidaComponent},
+  {path:'login', component:LoginComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    BienvenidaComponent
+    BienvenidaComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // RUTEO
+    RouterModule.forRoot(miRuteo)
   ],
   providers: [],
   bootstrap: [AppComponent]
