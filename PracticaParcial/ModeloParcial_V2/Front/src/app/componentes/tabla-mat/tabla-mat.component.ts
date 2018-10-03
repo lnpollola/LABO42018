f97c47778b64b960b24fbff588cd3f9036de88d0
lnpollola <leandro.pollola@hotmail.com>
2018-10-03 20:53:07 -0300

    OK TABLA FUNCIONANDO COMO COMPONENTE APARTE, LISTADO LLAMA A MAT TABLE, PROXIMO TRATAR DE PASAR PARAMETROS POR INPUT Y OUTPUT

--- PracticaParcial/ModeloParcial_V2/Front/src/app/componentes/tabla-mat/tabla-mat.component.ts
+++ PracticaParcial/ModeloParcial_V2/Front/src/app/componentes/tabla-mat/tabla-mat.component.ts
@@ -1,26 +1,12 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
-
+import { HeladosService } from '../../servicios/heladosService.service';
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  id: string;
+  sabor: string;
+  tipo: string;
+  kgrestantes: number;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 
 @Component({
   selector: 'app-tabla-mat',
@@ -30,24 +16,27 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 export class TablaMatComponent implements OnInit {
 
-  @Input('matColumnDef')
-  name: string;
-
-  @Input()
-sticky: boolean;
-
-@Input()
-stickyEnd: boolean;
-
-cssClassFriendlyName: string;
+  @Input('matColumnDef') name: string;
+  @Input() sticky: boolean;
+  @Input() stickyEnd: boolean;
 
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  cssClassFriendlyName: string;
+  displayedColumns: string[] = ['id', 'sabor', 'tipo', 'kgrestantes'];
+  listaHelados:any;
+  dataSource:any;
+  // dataSource = ELEMENT_DATA;
  
 
   
-  constructor() { }
+  constructor(private _servicio:HeladosService) 
+  {
+    this._servicio.ServiceTraerHelados().subscribe(data => {   
+      this.listaHelados = JSON.parse(data._body);
+      this.dataSource = this.listaHelados;
+    })
+
+   }
 
   ngOnInit() {
   }
